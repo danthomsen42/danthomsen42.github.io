26ff00455c66e82ed58e6bf35ae4edecb6398f50
danthomsen42 <danthomsen42@gmail.com>
2016-11-27 22:45:00 -0700

    Hopefully done

---
+++ homework/weather/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+            $("#page_header").load("/homework/weather/modules/header.html");
+            $("#page_footer").load("/homework/weather/modules/footer.html");
+            $("#navigation").load("/homework/weather/modules/nav.html");
+    });
