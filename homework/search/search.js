c316382bc63a2e375019b7d4a06f95533555d400
danthomsen42 <danthomsen42@gmail.com>
2016-12-02 21:53:08 -0700

    g

--- homework/search/search.js
+++ homework/search/search.js
@@ -1,22 +1,25 @@
 $('#query').keyup(function(){
   // All code will be inside of this block
-}); // end keyup
+
 
 var value = $('#query').val();
 
 var rExp = new RegExp(value, "i");
 
-$.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+$.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
   
         var output = '<ol>';
     $.each(data.RESULTS, function(key, val) {
       if (val.name.search(rExp) != -1) {
         output += '<li>';
-        output += '<a href="http://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+        output += '<a href="https://www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
         output += '</li>';
       }
-    
+   
      console.log(data);
     
   }); 
+     output += '</ol>';
+    $("#searchResults").html(output);
 });// end getJSON
+    }); // end keyup
