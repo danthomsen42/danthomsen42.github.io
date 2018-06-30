dfd74b6018037e9188d716b63c42d8d72c87bb8b
danthomsen42 <22300686+danthomsen42@users.noreply.github.com>
2018-06-30 00:00:11 -0600

    maybe..

---
+++ Dr-Vinyl-webpages/javascript/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+            $("#header-top").load("../header.html");
+            $("#footer-bottom").load("../footer.html");
+            //$("#navigation").load("/homework/weather/modules/nav.html");
+    });
